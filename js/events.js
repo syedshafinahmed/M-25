b6cb3153523944a32cb98e1b889e8d16cfea5ed2
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-19 21:04:57 +0600

    commit

--- js/events.js
+++ js/events.js
@@ -1,1 +1,23 @@
 console.log("External File");
+
+// Option 2 for event handler 
+function makeYellow() {
+    document.body.style.backgroundColor = 'yellow';
+}
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// Option 3 for event handler 
+const btnMakeBlue = document.getElementById('btn-make-blue');
+// console.log(btnMakeBlue);
+btnMakeBlue.onclick = function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+// Option 3.1 for event handler 
+const btnMakePurple = document.getElementById('btn-make-purple');
+btnMakePurple.onclick = makePurple;
+function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
